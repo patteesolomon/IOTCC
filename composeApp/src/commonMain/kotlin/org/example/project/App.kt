62c8d2c301b8450e83e4fd982bf957b58b78214b
patteesolomon <theshiningkillerrashakublade@outlook.com>
2025-07-27 22:13:19 -0500

    Add files via upload

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -2,6 +2,7 @@ package org.example.project
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
@@ -13,8 +14,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.sp
 import kotlinx.serialization.Serializable
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -23,6 +26,31 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 fun App() {
     MaterialTheme {
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = MainMenu
+        ){
+            composable<MainMenu>{
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(text = "hooray")
+                }
+            }
+            composable<Commands>{
+                val args = it.toRoute<Commands>()
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text(text = "hooray")
+                }
+            }
+        }
         var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
