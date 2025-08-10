cf94b6e4abe50d9abbceb8d6e0a64e43921a6eca
patteesolomon <theshiningkillerrashakublade@outlook.com>
2025-08-09 22:38:02 -0500

    Add files via upload

--- composeApp/Mongodb.kt
+++ composeApp/Mongodb.kt
@@ -3,17 +3,22 @@ package com.example.iotcc
 import com.mongodb.kotlin.client.coroutine.MongoClient
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import kotlinx.coroutines.runBlocking
+import org.bson.BsonType
+import org.bson.codecs.pojo.annotations.BsonCreator
 import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.codecs.pojo.annotations.BsonProperty
+import org.bson.codecs.pojo.annotations.BsonRepresentation
 import org.bson.types.ObjectId
 import org.slf4j.LoggerFactory
+import java.lang.System.getenv
 
-val CONNECTION_STRING = System.getenv("MONGO_URI")
+val CONNECTION_STRING = getenv("MONGO_URI")
 
     fun main(args: Array<String>){
-        val database: MongoDatabase = getDatabase()
-        val loggerParent = LoggerFactory.getLogger("parent")
-        val loggerChild = LoggerFactory.getLogger("parent.child")
-        runBlocking { addCommand(database) }
+        val database = getDatabase()
+        runBlocking {
+            println(database)
+        }
     }
     fun getDatabase(): MongoDatabase{
         val client = MongoClient.create(connectionString = CONNECTION_STRING)
@@ -22,13 +27,13 @@ val CONNECTION_STRING = System.getenv("MONGO_URI")
 
 data class CommandInfo(
     @BsonId
-    val id: ObjectId,
+    @BsonRepresentation(BsonType.OBJECT_ID)
+    @BsonProperty("type")
     val command: String
 )
 
 suspend fun addCommand(database: MongoDatabase){
     val info = CommandInfo(
-        id = ObjectId(),
         command = "sudo apt full-upgrade",
     )
     val collection = database.getCollection<CommandInfo>("commands")
